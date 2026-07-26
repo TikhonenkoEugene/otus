d3fd5b6623f2069be306fd78bdcc0afa844793ef
Eugeniy Tikhonenko <tikhonenko.eugene@gmail.com>
2026-07-27 01:27:28 +0700

    переделал короче так как не получилось дольше выполнить задания надеюсь что пойдет так как я не полнял задание описание супер краткое не понятно что использовать написано добааить я тупо добавил если не верно - пищите понятнее пожалуйста

---
+++ hw15-patterns/src/main/java/org/qadev/processor/ProcessorSwapFields.java
@@ -0,0 +1,10 @@
+package org.qadev.processor;
+
+import org.qadev.model.Message;
+
+public class ProcessorSwapFields implements Processor {
+    @Override
+    public Message process(Message message) {
+        return null;
+    }
+}
